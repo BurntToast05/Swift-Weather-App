16242724aae5d6b08fbe1d67ff5841450fe94569
BurntToast05 <corbin.gollaher@gmail.com>
2022-08-14 19:36:43 -0600

    Initial Commit

---
+++ Swift-Learning-App/Swift_Learning_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Learning_AppApp.swift
+//  Swift-Learning-App
+//
+//  Created by Corbin Gollaher on 8/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Learning_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
